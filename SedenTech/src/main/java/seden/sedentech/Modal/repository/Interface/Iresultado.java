55c6f7e81e5962132cbd4a0502a5e4c59e0ce072
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-21 22:14:27 -0300

    2 métodos com erro no Request no Postman

---
+++ SedenTech/src/main/java/seden/sedentech/Modal/repository/Interface/Iresultado.java
@@ -0,0 +1,8 @@
+package seden.sedentech.Modal.repository.Interface;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import seden.sedentech.Modal.beans.Resultado;
+
+
+public interface Iresultado extends JpaRepository<Resultado, String> {
+}
--- SedenTech/src/main/java/seden/sedentech/Resource/Routes/ResultadoRoute.java
+++ SedenTech/src/main/java/seden/sedentech/Resource/Routes/ResultadoRoute.java
@@ -1,5 +1,35 @@
 package seden.sedentech.Resource.Routes;
 
-public class ResultadoRoute {
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import seden.sedentech.Modal.beans.Resultado;
+import seden.sedentech.Modal.beans.Usuario;
+import seden.sedentech.Modal.repository.Interface.Iresultado;
+import seden.sedentech.Modal.repository.request.Resultado.RequestResultado;
+import seden.sedentech.Modal.repository.request.Usuario.RequestUsuario;
 
+@RestController
+@RequestMapping("/resultado")
+public class ResultadoRoute {
+    @Autowired
+    private Iresultado iresultado;
+
+    @Transactional
+    @GetMapping
+    public ResponseEntity GetResultado (){
+        var resultado = iresultado.findAll();
+        return ResponseEntity.ok(resultado);
+    }
+
+    @Transactional
+    @PostMapping
+    public ResponseEntity RegisterResultados(@RequestBody @Valid RequestResultado data){
+        Resultado newUsuario = new Resultado(data);
+        iresultado.save(newUsuario);
+
+        return ResponseEntity.ok(newUsuario);
+    }
 }
