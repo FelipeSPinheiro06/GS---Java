d4f3cf4e031f5f4576b0432c2f793c63c7519223
FelipePinheiro <fsp12371@git.com>
2023-11-24 22:26:03 -0300

    Final Commit

--- SedenTech/src/main/java/seden/sedentech/Model/BO/FeedbackBO.java
+++ SedenTech/src/main/java/seden/sedentech/Model/BO/FeedbackBO.java
@@ -14,37 +14,34 @@ public class FeedbackBO {
             f.setEstado("Bom");
             f.setConsultarMedico(false);
 
-            String mensagem = String.format("Seu resultado é %s! É recomendado você fazer %d exercícios por semana! "
-                            + "O seu estado é %s. Você deve consultar o médico com urgência? A resposta é %b. "
-                            + "Porém, se sentir algo, é sempre bom consultar um cardiologista.",
-                    diagnostico.getRiscoCardiaco(),
-                    f.getQtdExercicio(),
-                    f.getEstado(),
-                    f.isConsultarMedico());
+            String mensagem = "A sua chance de risco é baixa! É recomendado você fazer 1 exercícios por semana! "
+                            + "O seu estado é Bom. Você deve consultar o médico com urgência? A resposta é não. "
+                            + "Porém, se sentir algo, é sempre bom consultar um cardiologista.";
+
+
+            f.setMensagem(mensagem);
         }
         else if(diagnostico.getRiscoCardiaco() == "Moderado Risco") {
             f.setQtdExercicio(2);
             f.setEstado("Relativamente bom");
             f.setConsultarMedico(false);
 
-            String mensagem = String.format("Seu resultado é %s! É recomendado você fazer %d exercícios por semana! "
-                            + "O seu estado é %s. Você deve consultar o médico com urgência? A resposta é %b.",
-                    diagnostico.getRiscoCardiaco(),
-                    f.getQtdExercicio(),
-                    f.getEstado(),
-                    f.isConsultarMedico());
+            String mensagem = "A sua chance de risco é moderada! É recomendado você fazer 2 exercícios por semana! "
+                            + "O seu estado é Neutro. Você deve consultar o médico com urgência? A resposta é não, porém, é recomendado fazer uma consulta";
+
+
+            f.setMensagem(mensagem);
         }
         else if(diagnostico.getRiscoCardiaco() == "Alto Risco") {
             f.setQtdExercicio(4);
             f.setEstado("ruim");
             f.setConsultarMedico(true);
 
-            String mensagem = String.format("Seu resultado é %s! É recomendado você fazer %d exercícios por semana! "
-                            + "O seu estado é %s. Você deve consultar o médico com urgência? A resposta é %b.",
-                    diagnostico.getRiscoCardiaco(),
-                    f.getQtdExercicio(),
-                    f.getEstado(),
-                    f.isConsultarMedico());
+            String mensagem = "A sua chance de risco é Alta! É recomendado você fazer de 4 a 5 exercícios por semana! "
+                            + "O seu estado é ruim. Você deve consultar o médico com urgência? A resposta é sim. Porém, acalme-se, ainda há como reverter";
+
+
+            f.setMensagem(mensagem);
         }
 
         return f;
